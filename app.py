a1162af7f8af2da8de4069907b2ad16d86a0df5f
fctico11 <fac322@nyu.edu>
2025-02-24 21:58:43 -0600

    phase 10 setup

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request, redirect, url_for
+from game_logic import Game
+
+app = Flask(__name__)
+game = Game()
+
+@app.route("/")
+def home():
+    return render_template("game.html", game=game)
+
+@app.route("/draw", methods=["POST"])
+def draw_card():
+    if game.current_turn == "player":
+        from_discard = "from_discard" in request.form
+        game.draw_card("player", from_discard=from_discard)
+    return redirect(url_for("home"))
+
+@app.route("/select_card", methods=["POST"])
+def select_card():
+    card_index = int(request.form["card_index"])
+    game.select_card(card_index)
+    return redirect(url_for("home"))
+
+@app.route("/discard", methods=["POST"])
+def discard_card():
+    if game.current_turn == "player":
+        success = game.discard_selected_card()
+        if success and not game.skip_turn:
+            return redirect(url_for("computer_turn"))
+    return redirect(url_for("home"))
+
+@app.route("/computer_turn")
+def computer_turn():
+    if game.current_turn == "computer":
+        game.computer_turn()
+    return redirect(url_for("home"))
+
+@app.route("/submit_phase", methods=["POST"])
+def submit_phase():
+    game.submit_phase()
+    return redirect(url_for("home"))
+
+@app.route("/add_to_phase", methods=["POST"])
+def add_to_phase():
+    card_index = int(request.form["card_index"])
+    game.add_to_phase_attempt(card_index)
+    return redirect(url_for("home"))
+
+@app.route("/remove_from_phase", methods=["POST"])
+def remove_from_phase():
+    card_index = int(request.form["card_index"])
+    game.remove_from_phase_attempt(card_index)
+    return redirect(url_for("home"))
+
+@app.route("/hit_phase", methods=["POST"])
+def hit_phase():
+    card_index = int(request.form["card_index"])
+    game.hit_existing_phase(card_index)
+    return redirect(url_for("home"))
+
+@app.route("/reset_game", methods=["POST"])
+def reset_game():
+    game.reset_game()
+    return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ game_logic.py
@@ -0,0 +1,125 @@
+import random
+import collections
+
+COLORS = ["Red", "Blue", "Green", "Yellow"]
+NUMBERS = list(range(1, 13))
+WILD_COUNT = 8
+SKIP_COUNT = 4
+
+PHASES = [
+    {"sets": 2, "set_size": 3},  # Phase 1: Two sets of three
+    {"sets": 1, "set_size": 3, "run": 4},
+    {"sets": 1, "set_size": 4, "run": 4},
+    {"run": 7},
+    {"run": 8},
+    {"run": 9},
+    {"sets": 2, "set_size": 4},
+    {"color": 7},
+    {"sets": 1, "set_size": 5, "set_2": 2},
+    {"sets": 1, "set_size": 5, "set_2": 3},
+]
+
+class Card:
+    def __init__(self, color, number):
+        self.color = color
+        self.number = number
+
+    def is_wild(self):
+        return self.number == "Wild"
+
+    def is_skip(self):
+        return self.number == "Skip"
+
+def create_deck():
+    deck = [Card(color, num) for color in COLORS for num in NUMBERS for _ in range(2)]
+    deck += [Card(None, "Wild")] * WILD_COUNT
+    deck += [Card(None, "Skip")] * SKIP_COUNT
+    random.shuffle(deck)
+    return deck
+
+class Game:
+    PHASES = PHASES
+
+    def __init__(self):
+        self.reset_game()
+
+    def reset_game(self):
+        self.deck = create_deck()
+        self.discard_pile = [self.deck.pop()]
+        self.player_hand = [self.deck.pop() for _ in range(10)]
+        self.computer_hand = [self.deck.pop() for _ in range(10)]
+        self.player_phase = 0
+        self.computer_phase = 0
+        self.current_turn = "player"
+        self.has_drawn = False
+        self.skip_turn = False  
+        self.played_phases = {"player": [], "computer": []}
+        self.phase_submission_box = []  
+
+    def draw_card(self, player, from_discard=False):
+        if player == "player" and self.current_turn == "player" and not self.has_drawn:
+            if from_discard and self.discard_pile:
+                self.player_hand.append(self.discard_pile.pop())
+            elif self.deck:
+                self.player_hand.append(self.deck.pop())
+            self.has_drawn = True
+        elif player == "computer" and self.current_turn == "computer":
+            if self.deck:
+                self.computer_hand.append(self.deck.pop())
+
+    def add_card_to_phase_attempt(self, card_index):
+        if 0 <= card_index < len(self.player_hand):
+            card = self.player_hand.pop(card_index)
+            self.phase_submission_box.append(card)
+
+    def remove_card_from_phase_attempt(self, card_index):
+        if 0 <= card_index < len(self.phase_submission_box):
+            card = self.phase_submission_box.pop(card_index)
+            self.player_hand.append(card)
+
+    def check_phase_attempt(self):
+        phase_goal = PHASES[self.player_phase]
+        number_counts = collections.Counter(c.number for c in self.phase_submission_box if not c.is_wild())
+        wild_count = sum(1 for c in self.phase_submission_box if c.is_wild())
+
+        sets_needed = phase_goal.get("sets", 0)
+        set_size = phase_goal.get("set_size", 0)
+
+        formed_sets = 0
+        for num, count in sorted(number_counts.items(), key=lambda x: -x[1]):
+            if count >= set_size:
+                formed_sets += 1
+            elif count + wild_count >= set_size:
+                wild_count -= (set_size - count)
+                formed_sets += 1
+
+            if formed_sets >= sets_needed:
+                return True
+        return False
+
+    def submit_phase(self):
+        if self.check_phase_attempt():
+            self.played_phases["player"].extend(self.phase_submission_box)
+            self.phase_submission_box.clear()
+
+    def discard_card(self, card_index):
+        if 0 <= card_index < len(self.player_hand):
+            card = self.player_hand.pop(card_index)
+            self.discard_pile.append(card)
+            self.has_drawn = False
+
+            if card.is_skip():
+                self.skip_turn = True  
+                self.current_turn = "player"
+            else:
+                self.current_turn = "computer"
+
+            if not self.player_hand:
+                self.end_round("player")
+
+    def end_round(self, winner):
+        if winner == "player":
+            self.player_phase += 1
+        else:
+            self.computer_phase += 1
+        self.reset_game()
